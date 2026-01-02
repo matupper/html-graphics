b5ff0190ea5573b74e1f02b284adf9c361f45fa2
matupper <matias.a.tupper@gmail.com>
2026-01-01 22:23:24 -0800

    clear() and point()

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
+const BACKGROUND = "#000000";
+const FOREGROUND = "#50FF50";
+
 console.log(game);
 game.width = 800;
 game.height = 800;
 const ctx = game.getContext("2d");
 console.log(ctx);
-ctx.fillStyle = "#50FF50";
-ctx.fillRect(0, 0, 100, 100);
+
+function clear(){
+    ctx.fillStyle = BACKGROUND;
+    ctx.fillRect(0, 0, game.width, game.height);
+}
+
+function point (x, y){
+    ctx.fillStyle = FOREGROUND;
+    const s = 10;
+    
+    ctx.fillRect(x, y, s, s);
+}
+
+clear();
+point(100, 100);
